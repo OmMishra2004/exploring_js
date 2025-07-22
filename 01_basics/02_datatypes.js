412925b4bfa226824ab60596ff750889ec4bba9c
OmMishra2004 <om3107mishra@gmail.com>
2025-07-22 10:27:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict";// treat all JS code in accordance to the newer version of js
+let a =2// integer data type
+let b="om"//string data type
+let c=false// boolean data type
+let d;// undefined value matlab value future me hogi bas abhi define nai hai
+let e=null// null means empty value its type is object
+console.log(typeof undefined)// output=> undefined
+console.log(typeof null)// output=> object
