4726782f72c7790b207506d98eea284439e158f3
OmMishra2004 <om3107mishra@gmail.com>
2025-07-29 17:17:51 +0000

    numsandmaths

---
+++ 01_basics/06_numsandmath.js
@@ -0,0 +1,18 @@
+// const score=500;// here js automatically understands that a number is given 
+// const count= new Number(400)//here we explicitly define that this is a number
+// console.log(score,count);
+// console.log(score.toString().length);
+// console.log(score.toString().concat("hello"));
+
+const a=100.5265475
+console.log(a.toFixed(2));// Output:100.53
+console.log(a.toFixed(1));// Output:100.5
+console.log(a.toFixed(0));// Output:101
+// console.log(a.toFixed(-1)); not applicable 
+
+
+// const b=123.5698
+// console.log(b.toPrecision(4))// Output:123.6
+// console.log(b.toPrecision(3))// Output:124
+// console.log(b.toPrecision(2))// Output:1.2e+2
+
