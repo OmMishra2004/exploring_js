2de4326b4de80938e7806e11dd6346cb9a50c6a5
OmMishra2004 <om3107mishra@gmail.com>
2025-07-30 08:12:04 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,2 +1,11 @@
 const arr=[0,1,2,3,4,5]
 console.log(arr,typeof arr)
+// Array methods
+arr.push(10)// adds element at last
+console.log(arr);
+arr.pop()// remove element from the last 
+console.log(arr)
+arr.unshift(66)// add element at starting
+console.log(arr)
+arr.shift()// remove element at starting
+console.log(arr)
