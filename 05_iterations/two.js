b2c3da30c19ed22276f0f22acb28c968a125c93f
OmMishra2004 <om3107mishra@gmail.com>
2025-08-09 07:19:29 +0000

    whileanddowhiledone

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,12 +1,19 @@
 // while loop
-let i  = 1
-while (i<=10) {
-    console.log(i)
-    i++
-} 
-let myArray=["om","shiv",1,2]
-i=0
-while (i<myArray.length) {
-   console.log(myArray[i]) 
-   i++
-}
+// let i  = 1
+// while (i<=10) {
+//     console.log(i)
+//     i++
+// } 
+// let myArray=["om","shiv",1,2]
+// i=0
+// while (i<myArray.length) {
+//    console.log(myArray[i]) 
+//    i++
+// }
+
+// do while loop
+let a=11
+do {
+    console.log(a)
+    a+=2
+} while (a<=10);
