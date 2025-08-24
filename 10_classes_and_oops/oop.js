ce22486a20bd0ff3547c8bea4d6f5a4a5fb43c71
OmMishra2004 <om3107mishra@gmail.com>
2025-08-24 15:06:49 +0000

    oop

--- 10_classes_and_oops/oop.js
+++ 10_classes_and_oops/oop.js
@@ -1,8 +1,37 @@
-function userdetails(username,loginCount,isLoggedIn) {
-    this.username=username
-    this.loginCount=loginCount
-    this.isLoggedIn=isLoggedIn
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
     return this
 }
-const userOne=userdetails("Om",10,true)
-console.log(userOne)
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
